85d508f465948e5899ea8f21f49d2c3da5fd27af
Yarkın Çaktırma <ycaktirma@gmail.com>
2022-03-20 23:14:42 +0300

    ...

--- server.js
+++ server.js
@@ -9,18 +9,12 @@ server.use(router);
 
 server.listen(port);
 
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+app.use(express.static(dirname + '/dist/yarkinscoffeestore'));
+app.get('/*', function(req,res)
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/yarkinscoffeestore'));
+{res.sendFile(path.join(dirname+'/dist/yarkinscoffeestore/index.html'));});
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/yarkinscoffeestore/'}),
-);
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
