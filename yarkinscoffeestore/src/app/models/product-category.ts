35899d625c2644fd3f5fdf8e87adc3dfb506d689
ycaktirma <39536700+ycaktirma@users.noreply.github.com>
2022-03-21 02:32:53 +0300

    Delete yarkinscoffeestore directory

--- yarkinscoffeestore/src/app/models/product-category.ts
+++
@@ -1,8 +0,0 @@
-export class ProductCategory{
-    id:number;
-    category:string;
-    constructor(id:number, category:string){
-        this.id = id;
-        this.category = category;
-    }
-}
--- yarkinscoffeestore/src/app/models/user-login.ts
+++
@@ -1,9 +0,0 @@
-export class UserLogin{
-    email:string;
-    password:string;
-
-    constructor(email:string, password:string){
-        this.password = password;
-        this.email = email;
-    }
-}
