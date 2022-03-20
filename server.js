9c842732cf088e584e543de92fdc6ffda5a9423a
Yarkın Çaktırma <ycaktirma@gmail.com>
2022-03-20 23:05:06 +0300

    configuration...

--- server.js
+++ server.js
@@ -8,3 +8,19 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/yarkinscoffeestore'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/yarkinscoffeestore/index.html'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
